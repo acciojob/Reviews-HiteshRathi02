06d29179e9484e1c0776722c6e812e4016352018
Hitesh Rathi <97667730+HiteshRathi02@users.noreply.github.com>
2025-06-01 03:51:13 +0000

    Functionalities Added

---
+++ src/components/Review.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Review ({review , handlePrevBtn, handleNextBtn, handleRandBtn}){
+    return(
+        <>
+        <div className="review">
+            <p className="author" author-id={review.id}>{review.name}</p>
+            <p className="job">{review.job}</p>
+            <p className="info">{review.text}</p>
+            <img className="person-img" style={{ width:100}} src={review.image}></img>
+            <button className="prev-btn" onClick={()=>handlePrevBtn()}>Previous</button>
+            <button className="next-btn" onClick={()=>handleNextBtn()}>Next</button>
+            <button className="random-btn" onClick={()=>handleRandBtn()}>Surprise Me</button>
+        </div>
+        </>
+    )
+}
